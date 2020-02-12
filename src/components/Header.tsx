5e8640a86d2614c93c25504bc6423df9d0950b53
MaxErshov <maxx95.95@gmail.com>
2020-02-12 22:35:19 +0300

    Start handler for dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,13 @@ import * as React from "react";
 import { Link, Redirect } from "react-router-dom";
 import * as burgerImg from '../images/burger.svg';
 
+function chgNight():void {
+  const doc = document.getElementsByClassName('app')[0] as HTMLElement;
+  console.log(doc);
+  doc.className="app_night";
+}
 
-
-const Header: React.FC = () => {
+const Header: React.FC = () => {  
   const [openMenu, chgMenu] = React.useState<boolean>(false);
   return (
     <header>
@@ -15,6 +19,7 @@ const Header: React.FC = () => {
         <Link to="/dev">PROJECTS</Link>
         <Link to="/contacts">CONTACTS</Link>
       </nav>
+      <button type="button" onClick={() => chgNight()}>Night</button>
     </header>
   );
 }
