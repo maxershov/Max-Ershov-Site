1cd073316126dfcb6b3dfc477bcb9fb6bc43efc2
Max Ershov <maxx95.95@gmail.com>
2020-02-13 16:47:32 +0300

    Add form handler and chg tsconf to es6

--- src/components/ContactFields.tsx
+++ src/components/ContactFields.tsx
@@ -4,9 +4,9 @@ import * as React from 'react';
 const ContactFields: React.FC = () => (
   <div className="contacts__left">
     <p>Contact me:</p>
-    <form>
-      <input minLength={5} placeholder="Contact adress" type="text" />
-      <textarea minLength={5} placeholder="Message" />
+    <form action="https://formspree.io/xbjowqpg" method="POST">
+      <input minLength={5} name="_replyto" placeholder="Contact adress" type="email" />
+      <textarea name="message" minLength={5} placeholder="Message" />
       <button type="submit">SEND MESSAGE</button>
     </form>
     <button type="button">JUST SAY HI</button>
