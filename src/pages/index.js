84f2b565da98ab524231447450c895a7b85029ee
Max Ershov <maxx95.95@gmail.com>
2020-10-08 21:15:17 +0300

    feat: add react-helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import '../assets/App.scss';
 import MainPage from '../components/MainPage';
 import Header from '../components/Header';
 import ProjectsPage from '../components/ProjectsPage';
 import ContactsPage from "../components/ContactsPage";
-
 import Context from '../components/Context';
-
 import { updateL18n } from '../components/state';
+import siteImg from '../images/site.jpg'
 
 
 export default function Home() {
@@ -36,6 +36,26 @@ export default function Home() {
 
   return <div>
     <Context.Provider value={data}>
+      <Helmet>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="title" content="Max's Portfolio" />
+      <link rel="manifest" href="site.webmanifest" />
+      <link rel='icon' href='favicon.ico' type='image/x-icon' />
+      <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+      <meta name="description" content="Hi! Check out my projects and contact me here" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://maksershov.ru/" />
+      <meta property="og:title" content="Max's Portfolio" />
+      <meta property="og:description" content="Hi! Check out my projects and contact me here" />
+      <meta property="og:image" content={`https://maksershov.ru/${siteImg}`} />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://maksershov.ru/" />
+      <meta property="twitter:title" content="Max's Portfolio" />
+      <meta property="twitter:description" content="Hi! Check out my projects and contact me here" />
+      <meta property="twitter:image"  content={`https://maksershov.ru/${siteImg}`} />
+      </Helmet>
       <Header setLng={setLng} />
       <MainPage></MainPage>
       {data.loading ? null : (<ProjectsPage />)}
